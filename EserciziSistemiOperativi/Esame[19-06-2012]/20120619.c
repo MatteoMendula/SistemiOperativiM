a10d2c1c733c22f48b2311ae4355250aa0f320c9
Marco Moschettini <m.marmos@gmail.com>
2015-01-15 21:22:58 +0100

    Qualche problema di deadlock sull'esercizio delle piastrelle.

--- EserciziSistemiOperativi/Esame[19-06-2012]/20120619.c
+++ EserciziSistemiOperativi/Esame[19-06-2012]/20120619.c
@@ -7,122 +7,247 @@
 //  gcc -D_REENTRANT -std=c99 -o Esame Esame.c -lpthread
 //
 
-#include "Template_Monitor.h"
+#include "20120619.h"
 
-void * thread1(void * t)
+void * lotto_standard(void * t)
 {
-    entra(&monitor);
-    sleep(2);
-    esce(&monitor);
+    Lotto lotto;
+    lotto.dimensione_lotto = N_LOTTO;
+    lotto.tempo_cottura = rand() % 5;
+    lotto.tipo = rand()%2; //tipo T1 o T2
+    
+    switch(lotto.tipo)
+    {
+        case T1:
+            entra_standard_t1(&forno, &lotto);
+            sleep(lotto.tempo_cottura);
+            esce_standard_t1(&forno, &lotto);
+            break;
+        case T2:
+            entra_standard_t2(&forno, &lotto);
+            sleep(lotto.tempo_cottura);
+            esce_standard_t2(&forno, &lotto);
+            break;
+    }
     pthread_exit(NULL);
 }
 
-void * thread2(void * t)
+void * lotto_speciale(void * t)
 {
-    //esempio con memoria
-    int scelta;
+    Lotto lotto;
+    lotto.dimensione_lotto = N_LOTTO;
+    lotto.tempo_cottura = rand() % 5;
+    lotto.tipo = rand() % 2; //T1 o T2
     
-    entra2(&monitor, &scelta);
-    sleep(2);
-    esce2(&monitor, &scelta);
-    
+    switch(lotto.tipo)
+    {
+        case T1:
+            entra_special_t1(&forno, &lotto);
+            sleep(lotto.tempo_cottura);
+            esce_special_t1(&forno, &lotto);
+            break;
+        case T2:
+            entra_special_t2(&forno, &lotto);
+            sleep(lotto.tempo_cottura);
+            esce_special_t2(&forno, &lotto);
+            break;
+    }
     pthread_exit(NULL);
 }
 
-void entra(Monitor * m)
+int totale_lotti_in_forno(Forno * f)
 {
-    pthread_mutex_lock(&m->lock);
+    return f->lotti_special_t1_forno + f->lotti_special_t2_forno + f->lotti_standard_t1_forno + f->lotti_standard_t2_forno;
+}
+
+void risveglio(Forno * f)
+{
+    if(f->lotti_standard_t1_in_coda > 0 || f->lotti_standard_t2_in_coda > 0)
+        pthread_cond_signal(&f->standard_coda);
+    else if(f->lotti_special_t1_in_coda > 0 || f->lotti_special_t2_in_coda > 0)
+        pthread_cond_signal(&f->special_coda);
+    
+}
+
+void entra_standard_t1(Forno * f, Lotto * l)
+{
+    pthread_mutex_lock(&f->lock);
+    
     //condizione di blocco
-    while(1)
+    // - se il forno è pieno
+    // - se in forno ci sono l'altro tipo di piastrelle
+    while((totale_lotti_in_forno(f) == MAX_PIASTRELLE_FORNO) || (f->lotti_special_t2_forno > 0) || (f->lotti_standard_t2_forno))
     {
-        m->n_in_coda1++;
-        pthread_cond_wait(&m->coda1, &m->lock);
-        m->n_in_coda1--;
+        printf("[Lotto standard T1] Sospendo\n");
+        f->lotti_standard_t1_in_coda++;
+        pthread_cond_wait(&f->standard_coda, &f->lock);
+        printf("[Lotto standard T1] Mi risveglio\n");
+        f->lotti_standard_t1_in_coda--;
     }
     
     //cose da fare quando mi sblocco
-    
+    f->lotti_standard_t1_forno++;
+    printf("[Lotto standard T1] Entrato in forno\n");
+
     //risveglio secondo priorità
-    if(m->n_in_coda1 > 0)
-        pthread_cond_signal(&m->coda1);
-    else if(m->n_in_coda2 > 0)
-        pthread_cond_signal(&m->coda2);
+    risveglio(f);
     
-    pthread_mutex_unlock(&m->lock);
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    pthread_mutex_unlock(&f->lock);
 }
 
-void esce(Monitor * m)
+void entra_special_t1(Forno * f, Lotto * l)
 {
-    pthread_mutex_lock(&m->lock);
+    pthread_mutex_lock(&f->lock);
     
-    //cose da fare
-    
-    //risveglio secondo priorità
-    if(m->n_in_coda1 > 0)
-        pthread_cond_signal(&m->coda1);
-    else if(m->n_in_coda2 > 0)
-        pthread_cond_signal(&m->coda2);
-    
-    pthread_mutex_unlock(&m->lock);
-}
-
-void entra2(Monitor *m, int * scelta)
-{
-    pthread_mutex_lock(&m->lock);
     //condizione di blocco
-    while(1)
+    // - se il forno è pieno
+    // - se c'è in coda uno standard
+    // - se in forno ci sono l'altro tipo di piastrelle
+    while((totale_lotti_in_forno(f) == MAX_PIASTRELLE_FORNO) || (f->lotti_standard_t1_in_coda > 0) || (f->lotti_standard_t2_in_coda > 0) || (f->lotti_special_t2_forno > 0) || (f->lotti_standard_t2_forno))
     {
-        m->n_in_coda2++;
-        pthread_cond_wait(&m->coda2, &m->lock);
-        m->n_in_coda2--;
+        printf("[Lotto special T1] Sospendo\n");
+        f->lotti_special_t1_in_coda++;
+        pthread_cond_wait(&f->standard_coda, &f->lock);
+        printf("[Lotto special T1] Mi risveglio\n");
+        f->lotti_standard_t1_in_coda--;
     }
     
     //cose da fare quando mi sblocco
+    f->lotti_standard_t1_forno++;
+    printf("[Lotto standard T1] Entrato in forno\n");
     
-    //salvo la scelta! All'uscita se lo ricorderà
-    if(1)
-        *scelta = 1;
-    else if(0)
-        *scelta = 0;
-    
-    
-    //risveglio secondo priorità (in questo caso priorità coda1)
-    if(m->n_in_coda1 > 0)
-        pthread_cond_signal(&m->coda1);
-    else if(m->n_in_coda2 > 0)
-        pthread_cond_signal(&m->coda2);
-    
-    pthread_mutex_unlock(&m->lock);
+    risveglio(f);
+
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    //risveglio secondo priorità
+    pthread_mutex_unlock(&f->lock);
 }
 
-void esce2(Monitor * m, int * scelta)
+
+void esce_standard_t1(Forno * f, Lotto * l)
 {
-    pthread_mutex_lock(&m->lock);
+    pthread_mutex_lock(&f->lock);
     
-    //cose da fare
-    if(*scelta == 1)
-        printf("blabla");
-    else if(*scelta == 2)
-        printf("babab");
+    f->lotti_standard_t1_forno--;
+    printf("[Lotto standard T1] Uscito dal forno\n");
     
     //risveglio secondo priorità
-    if(m->n_in_coda1 > 0)
-        pthread_cond_signal(&m->coda1);
-    else if(m->n_in_coda2 > 0)
-        pthread_cond_signal(&m->coda2);
-    
-    pthread_mutex_unlock(&m->lock);
+    risveglio(f);
+
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    pthread_mutex_unlock(&f->lock);
 }
 
-
-void init_monitor(Monitor * monitor)
+void esce_special_t1(Forno * f, Lotto * l)
 {
-    pthread_mutex_init(&monitor->lock, NULL);
-    pthread_cond_init(&monitor->coda1, NULL);
-    pthread_cond_init(&monitor->coda2, NULL);
+    pthread_mutex_lock(&f->lock);
     
-    monitor->n_in_coda1 = 0;
-    monitor->n_in_coda2 = 0;
+    f->lotti_special_t1_forno--;
+    printf("[Lotto special T1] Uscito dal forno\n");
+    
+    //risveglio secondo priorità
+    risveglio(f);
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    pthread_mutex_unlock(&f->lock);
+}
+
+void entra_standard_t2(Forno * f, Lotto * l)
+{
+    pthread_mutex_lock(&f->lock);
+    
+    //condizione di blocco
+    // - se il forno è pieno
+    // - se in forno ci sono l'altro tipo di piastrelle
+    while((totale_lotti_in_forno(f) == MAX_PIASTRELLE_FORNO) || (f->lotti_special_t1_forno > 0) || (f->lotti_standard_t1_forno))
+    {
+        printf("[Lotto standard T2] Sospendo\n");
+        f->lotti_standard_t2_in_coda++;
+        pthread_cond_wait(&f->standard_coda, &f->lock);
+        printf("[Lotto standard T2] Mi risveglio\n");
+        f->lotti_standard_t2_in_coda--;
+    }
+    
+    //cose da fare quando mi sblocco
+    f->lotti_standard_t2_forno++;
+    printf("[Lotto standard T1] Entrato in forno\n");
+    
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    //risveglio secondo priorità
+    risveglio(f);
+    
+    pthread_mutex_unlock(&f->lock);
+}
+
+void entra_special_t2(Forno * f, Lotto * l)
+{
+    pthread_mutex_lock(&f->lock);
+    
+    //condizione di blocco
+    // - se il forno è pieno
+    // - se c'è in coda uno standard
+    // - se in forno ci sono l'altro tipo di piastrelle
+    while((totale_lotti_in_forno(f) == MAX_PIASTRELLE_FORNO) || (f->lotti_standard_t1_in_coda > 0) || (f->lotti_standard_t2_in_coda > 0) || (f->lotti_special_t1_forno > 0) || (f->lotti_standard_t1_forno))
+    {
+        printf("[Lotto special T2] Sospendo\n");
+        f->lotti_special_t2_in_coda++;
+        pthread_cond_wait(&f->standard_coda, &f->lock);
+        printf("[Lotto special T2] Mi risveglio\n");
+        f->lotti_standard_t2_in_coda--;
+    }
+    
+    //cose da fare quando mi sblocco
+    f->lotti_standard_t2_forno++;
+    printf("[Lotto standard T2] Entrato in forno\n");
+    
+    risveglio(f);
+
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    //risveglio secondo priorità
+    pthread_mutex_unlock(&f->lock);
+}
+
+void esce_standard_t2(Forno * f, Lotto * l)
+{
+    pthread_mutex_lock(&f->lock);
+    
+    f->lotti_standard_t2_forno--;
+    printf("[Lotto standard T2] Uscito dal forno\n");
+    
+    //risveglio secondo priorità
+    risveglio(f);
+
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    pthread_mutex_unlock(&f->lock);
+}
+
+void esce_special_t2(Forno * f, Lotto * l)
+{
+    pthread_mutex_lock(&f->lock);
+    
+    f->lotti_special_t2_forno--;
+    printf("[Lotto special T2] Uscito dal forno\n");
+    
+    //risveglio secondo priorità
+    risveglio(f);
+
+    printf("[STATO] %d special t1, %d special t2, %d standard t1, %d standard t2 in forno. %d special t1, %d special t2, %d standard t1, %d standard t2 in coda\n", f->lotti_special_t1_forno, f->lotti_special_t2_forno, f->lotti_standard_t1_forno, f->lotti_standard_t2_forno,f->lotti_special_t1_in_coda, f->lotti_special_t2_in_coda, f->lotti_standard_t1_in_coda, f->lotti_standard_t2_in_coda);
+    pthread_mutex_unlock(&f->lock);
+}
+
+void init_forno(Forno * forno)
+{
+    pthread_mutex_init(&forno->lock, NULL);
+    pthread_cond_init(&forno->standard_coda, NULL);
+    pthread_cond_init(&forno->special_coda, NULL);
+    forno->lotti_special_t1_in_coda = 0;
+    forno->lotti_standard_t1_in_coda = 0;
+    forno->lotti_special_t2_in_coda = 0;
+    forno->lotti_standard_t2_in_coda = 0;
+    forno->lotti_standard_t1_forno = 0;
+    forno->lotti_special_t2_forno = 0;
+    forno->lotti_special_t1_forno = 0;
+    forno->lotti_special_t2_forno = 0;
 }
 
 int main(int argc, char *  argv[])
@@ -132,11 +257,11 @@ int main(int argc, char *  argv[])
     
     printf("Programma avviato.\n");
     
-    init_monitor(&monitor);
+    init_forno(&forno);
     
-    for(int i = 0; i < THREADS; i++)
+    for(int i = 0; i < STANDARD_N; i++)
     {
-        result = pthread_create(&threads[i], NULL, thread1, (void *)(intptr_t) i);
+        result = pthread_create(&lotti_standard[i], NULL, lotto_standard, (void *)(intptr_t) i);
         if(result)
         {
             printf("Errore nella creazione del thread %d con stato %d\n", i, result);
@@ -144,12 +269,29 @@ int main(int argc, char *  argv[])
         }
     }
     
-    //avvio di ulteriori threads
+    for(int i = 0; i < SPECIAL_N; i++)
+    {
+        result = pthread_create(&lotti_special[i], NULL, lotto_speciale, (void *)(intptr_t) i);
+        if(result)
+        {
+            printf("Errore nella creazione del thread %d con stato %d\n", i, result);
+            exit(-1);
+        }
+    }
     
     //join dei threads
-    for(int i = 0; i < THREADS; i++)
+    for(int i = 0; i < STANDARD_N; i++)
     {
-        result = pthread_join(threads[i], status);
+        result = pthread_join(lotti_standard[i], status);
+        if(result)
+        {
+            printf("Errore nel join del thread %d con stato %d e risultato %d\n", i, result, (int)(intptr_t)status);
+        }
+    }
+    
+    for(int i = 0; i < SPECIAL_N; i++)
+    {
+        result = pthread_join(lotti_special[i], status);
         if(result)
         {
             printf("Errore nel join del thread %d con stato %d e risultato %d\n", i, result, (int)(intptr_t)status);
--- EserciziSistemiOperativi/Esame[19-06-2012]/20120619.h
+++ EserciziSistemiOperativi/Esame[19-06-2012]/20120619.h
@@ -10,34 +10,67 @@
 #include <semaphore.h>
 
 //DEFINIZIONI
-#define THREADS 10
+#define STANDARD_N 10
+#define SPECIAL_N 5
+#define MAX_PIASTRELLE_FORNO 30
+#define N_LOTTO 5
+#define T1 0
+#define T2 1
 
 //DICHIARAZIONI MONITOR
 
 typedef struct
 {
+    int dimensione_lotto;
+    int tempo_cottura;
+    int tipo;
+    
+}Lotto;
+
+typedef struct
+{
     pthread_mutex_t lock;
-    pthread_cond_t coda1;
-    pthread_cond_t coda2;
+    pthread_cond_t standard_coda;
+    pthread_cond_t special_coda;
     
-    int n_in_coda1;
-    int n_in_coda2;
+    int lotti_special_t1_in_coda;
+    int lotti_standard_t1_in_coda;
     
-} Monitor;
+    int lotti_special_t2_in_coda;
+    int lotti_standard_t2_in_coda;
+    
+    int lotti_standard_t1_forno;
+    int lotti_standard_t2_forno;
+    int lotti_special_t1_forno;
+    int lotti_special_t2_forno;
+    
+} Forno;
 
 //DICHIARAZIONI VARIABILI
-pthread_t threads[THREADS];
-Monitor monitor;
+pthread_t lotti_standard[STANDARD_N];
+pthread_t lotti_special[SPECIAL_N];
+Forno forno;
 
 //FUNZIONI
-void init_monitor(Monitor *);
-void entra(Monitor *);
-void esce(Monitor *);
-void entra2(Monitor *, int *);
-void esce2(Monitor *, int *);
+void init_forno(Forno *);
+void entra_standard_t1(Forno *, Lotto *);
+void entra_standard_t2(Forno *, Lotto *);
+
+void entra_special_t1(Forno *, Lotto *);
+void entra_special_t2(Forno *, Lotto *);
+
+void esce_standard_t1(Forno *, Lotto *);
+void esce_standard_t2(Forno *, Lotto *);
+
+void esce_special_t1(Forno *, Lotto *);
+void esce_special_t2(Forno *, Lotto *);
+
+void risveglio(Forno *);
+
+int totale_lotti_in_forno(Forno *);
 
 //THREADS
-void * thread1(void *);
-void * thread2(void *);
+void * lotto_standard(void *);
+void * lotto_speciale(void *);
 
 #endif
